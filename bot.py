a3ad94d61c5a0cd47c643ed456eb70dc01e84ac9
GOJO247 <138173607+GOJO247@users.noreply.github.com>
2025-02-14 14:04:30 -0500

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
 bot_telegram -m python3.12 venv .venv
-ot_telegram source .venv/bin/activate 
-(.venv) bot_telegram pip install python-telegram-bot
+bot_telegram source .venv/bin/activate 
+(.venv) 
+bot_telegram pip install python-telegram-bot
